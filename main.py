99a8bb0924daac19dde3ed8bbc014a4af8f25749
Cray077 <bernando2345@gmail.com>
2023-06-11 09:21:12 +0700

    changed the display question and answer to a func

--- main.py
+++ main.py
@@ -15,11 +15,18 @@ questionFrame.grid(row=0, column=1, sticky="n")
 answerFrame = Frame(root)
 answerFrame.grid(row=1, column=1)
 
+
+def display_quiz(question, answer):
+    # display the question
+    questionLabel = Label(questionFrame, text=question)
+    questionLabel.grid(row=0, column=0)
+    # display the answer
+    for (text, answer) in answer.items():
+        radioButton = Radiobutton(answerFrame, text=text, value=answer, width=40, indicatoron=0, padx=10)
+        radioButton.pack()
+
+
 question = "Berapakah angka tertinggi dalam 8 bit ?"
-
-questionLabel = Label(questionFrame, text=question)
-questionLabel.grid(row=0, column=0)
-
 answer = {
     "255" : "255",
     "256" : "256",
@@ -28,19 +35,6 @@ answer = {
     "259" : "259"
 }
 
-for (text, answer) in answer.items():
-    radioButton = Radiobutton(answerFrame, text=text, value=answer, width=40, indicatoron=0, padx=10)
-    radioButton.pack()
-
-
-
-
-
-
-
-
-
-
-
+display_quiz(question, answer)
 
 root.mainloop()
